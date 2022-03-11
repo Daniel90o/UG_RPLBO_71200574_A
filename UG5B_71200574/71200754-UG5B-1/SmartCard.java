5b0d1de29243ad83dbda8a0129daa8f684c94f40
DanielAdi <79075886+Daniel90o@users.noreply.github.com>
2022-03-11 09:51:15 +0700

    Add files via upload

---
+++ UG5B_71200574/71200754-UG5B-1/SmartCard.java
@@ -0,0 +1,21 @@
+package com.ug5b.soal1;
+import java.util.Scanner;
+
+public class SmartCard {
+    String nama;
+    long saldo;
+
+    String getNama(){return nama;}
+
+    long getSaldo(){return saldo;}
+
+    public static void setNama(String nama){
+        System.out.println (nama);}
+
+    public static void setSaldo(long saldo){
+        System.out.println(saldo);}
+
+    void topUp(long topUp){
+
+    }
+}
---
+++ UG5B_71200574/71200754-UG5B-1/Voucher.java
@@ -0,0 +1,17 @@
+public class Voucher {
+    String Voucher;
+    long nominal;
+
+    void setVoucher(String voucher){
+        this.voucher=voucher;
+    }
+    String getVoucher(){
+        return Voucher;
+    }
+    void setNominal(long nominal){
+        this.nominal=nominal;
+    }
+    long getNominal(){
+        return nominal;
+    }
+}
